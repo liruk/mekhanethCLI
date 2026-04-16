7a8782bc5b253f943d8cbe0437c0fbe34885b326
liruk <34234523+liruk@users.noreply.github.com>
2026-04-16 17:55:59 +0900

    add vivliostyle(just sample...)

---
+++ vivliostyle.config.cjs
@@ -0,0 +1,13 @@
+// @ts-check
+/** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
+module.exports = {
+  title: "「禁断のオフィス百合えっち」特典小説",
+  author: "獄甘の篝火",
+  language: "ja",
+  browser: "chrome@145.0.7632.26",
+  image: "ghcr.io/vivliostyle/cli:10.3.1",
+  theme: ["@vivliostyle/theme-bunko", "./vivliostyle.css"],
+  entry: [
+    "./products/20260301「カワイイスパイスの性欲処理-3：禁断のオフィス百合えっち」.md",
+  ],
+};
